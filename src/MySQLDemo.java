44d9706446b79a5b697ef8d4f9ad6ee07600ec8e
duckdoer <avikdeb@duckdoer.net.in>
2025-03-01 06:46:14 +0530

    # sample code for basic JDBC connection.

---
+++ src/MySQLDemo.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MySQLDemo {
+    public static void main(String[] args) {
+        Connection connection = null;
+        String url = "jdbc:mysql://localhost:3306/perldemo";
+        String user = "root";
+        String password = "root";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM perldemo.emp;");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id"));
+                System.out.println(resultSet.getString("firstname"));
+                System.out.println(resultSet.getString("lastname"));
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
